326a867c61757b46ef0c73b8db574303ae97242c
Sobiwa <kevin.b.collister@gmail.com>
2022-10-20 14:48:34 -0400

    Edit buttons
    switch position and lower

--- script.js
+++ script.js
@@ -304,7 +304,7 @@ const endGame = (function () {
         location.reload();
     })
     characterSelectButton.textContent = 'Character Select';
-    buttonContainer.append(playAgainButton, characterSelectButton);
+    buttonContainer.append(characterSelectButton, playAgainButton);
     const winPossibilities = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]];
     let strike;
 
